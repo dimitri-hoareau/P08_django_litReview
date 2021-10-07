8efc69efdc03f33de3469bea14acc74e1fa36773
dimitri-hoareau <dimitrihoareau2@gmail.com>
2021-10-07 15:16:48 +0200

    get tickets and reviews for flux page

--- LitReview/flux/models.py
+++ LitReview/flux/models.py
@@ -1,15 +1,19 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
-
+from django.utils import timezone
 
 class Ticket(models.Model):
-    # Your Ticket model definition goes here
-    pass
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
+    time_created = models.DateTimeField(default=timezone.now)
 
 
 class Review(models.Model):
-    # ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE, default='')
     rating = models.PositiveSmallIntegerField(
         # validates that rating must be between 0 and 5
         validators=[MinValueValidator(0), MaxValueValidator(5)])
--- LitReview/flux/views.py
+++ LitReview/flux/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from .models import Review
+from .models import Review, Ticket
+
+import logging
+log = logging.getLogger()
 
 def post_list(request):
+
+    list_of_T_and_R = []
     reviews = Review.objects.order_by('-time_created')
-    return render(request, 'flux/reviews_list.html', {'reviews': reviews})
+    tickets = Ticket.objects.order_by('-time_created')
+    for review in reviews:
+        list_of_T_and_R.append(review)
+    for ticket in tickets:
+        list_of_T_and_R.append(ticket)
+
+    ordonnerd_list_of_T_and_R = sorted(list_of_T_and_R, key=lambda k: k.time_created, reverse=True)
+
+    for element in ordonnerd_list_of_T_and_R:
+        print(element)
+
+    return render(request, 'flux/reviews_list.html', {'reviews': reviews, "tickets": tickets, "list_of_ticket_and_reviews": ordonnerd_list_of_T_and_R})
